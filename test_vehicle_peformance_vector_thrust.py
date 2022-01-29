e939e72090261cad0bdb2adb0c2eb539c065a37c
mrinmoy sarkar <mrinmoy.pol@gmail.com>
2022-01-29 13:41:09 -0500

    copy utility function is added
    Signed-off-by: mrinmoy sarkar <mrinmoy.pol@gmail.com>

--- test_vehicle_peformance_vector_thrust.py
+++ test_vehicle_peformance_vector_thrust.py
@@ -731,8 +731,7 @@ def main():
     print(configs.base.weight_breakdown)
     print(configs.base.mass_properties.center_of_gravity)
 
-    # Plot vehicle 
-    plot_vehicle(configs.cruise, save_figure = False, plot_control_points = False)
+    
 
     # evaluate mission    
     mission  = analyses.missions.base
@@ -740,30 +739,7 @@ def main():
 
     # plot results
     plot_mission(results)   
-
-    # save, load and plot old results 
-    #save_tiltwing_results(results)
-    old_results = load_tiltwing_results() 
-    plot_mission(old_results,'k-')
-
-    # RPM check during hover
-    RPM        = results.segments.departure.conditions.propulsion.propeller_rpm[0][0]
-    RPM_true   = 1926.3247780885956
-    
-    print(RPM) 
-    diff_RPM   = np.abs(RPM - RPM_true)
-    print('RPM difference')
-    print(diff_RPM)
-    assert np.abs((RPM - RPM_true)/RPM_true) < 1e-3  
-
-    # lift Coefficient Check During Cruise
-    lift_coefficient        = results.segments.climb.conditions.aerodynamics.lift_coefficient[0][0] 
-    lift_coefficient_true   = 1.0211063664785844
-    print(lift_coefficient)
-    diff_CL                 = np.abs(lift_coefficient  - lift_coefficient_true) 
-    print('CL difference')
-    print(diff_CL)
-    assert np.abs((lift_coefficient  - lift_coefficient_true)/lift_coefficient_true) < 1e-3    
+  
 
 
     return
---
+++ utility_to_copy_trajectories.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  6 12:15:09 2021
+
+@author: mrinmoy sarkar
+"""
+
+# ----------------------------------------------------------------------
+#   Imports
+# ----------------------------------------------------------------------
+import sys
+import numpy as np
+import pandas as pd
+import time
+import os
+import shutil
+
+if __name__ == "__main__":
+    all_UTM_data_df = pd.read_csv('./logs/sampled_UTM_dataset.csv')
+    input_path = './logs/all_trajectories/'
+    destination = './logs/sampled_trajectories/'
+    for row_id in range(all_UTM_data_df.shape[0]):
+        agent_id = all_UTM_data_df.iloc[row_id]['agent_id']
+        source = input_path+'Trajectory_' + str(agent_id) + '.csv'
+        shutil.copy(source, destination)
